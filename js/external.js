9e4f4b474d500cebf62000fad5ff693ae0ccc888
Julia Contreras <juliacontreras483@gmail.com>
2020-04-21 08:58:50 -0500

    js

---
+++ js/external.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log('Hello from external JavaScript!');
+
+
+alert("Welcome to my Website!");
+
+
+var userInput = prompt('What is your favorite color?:');
+console.log('Great, ' + userInput + ' is my favorite color too!');
+
+
+
+
+/*Write some JavaScript code, that is, variables and operators, to describe the following scenarios. Do not worry about the real operations to get the values, the goal of these exercises is to understand how real world conditions can be represented with code.
+
+You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
+Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
+A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
+A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products. */
