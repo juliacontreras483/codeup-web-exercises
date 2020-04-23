9d977db072bb28b7b377630522fd741dbbf9b645
Julia Contreras <juliacontreras483@gmail.com>
2020-04-22 23:11:12 -0500

    js practice

---
+++ js/miniproblems.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+// ===== MINI PROBLEMS
+// Write a function, returnFive, that returns the number five. No inputs should be defined.
+// Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+// Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+// Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+// Write a function, getSmallerSegment, that takes in a string and a number input. The function should return a substring of the first argument that is as many characters long as the second argument in lowercase.
+// example input: getSmallerSegment("Codeup", 3)
+// example output: "cod"
