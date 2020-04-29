b744bf15c0bb45a16b95435e3749656300c2d3a5
Julia Contreras <juliacontreras483@gmail.com>
2020-04-28 21:40:10 -0500

    work so far

--- js/miniproblems.js
+++ js/miniproblems.js
@@ -3,9 +3,51 @@
 
 // ===== MINI PROBLEMS
 // Write a function, returnFive, that returns the number five. No inputs should be defined.
+
+function returnFive() {
+    return 5;
+}
+
+
+
 // Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
+//
+// function isFive(input) {
+//     if (input === 5 || input === "5"){
+//         return true
+//     }else{
+//         return false
+//     }
+//
+// }
+
+function isFive(input) {
+    return (input === 5 || input === "5");
+
+}
+
+
+
+console.log(isFive(6));
+console.log(isFive("8"));
+
 // Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
+
+function isShortWord(string) {
+
+}
+
 // Write a function, isSameLength, that takes in two string inputs and returns the boolean value true if the passed arguments are the same length. Return false otherwise.
+
+function isSameLength(input) {
+
+}
+
 // Write a function, getSmallerSegment, that takes in a string and a number input. The function should return a substring of the first argument that is as many characters long as the second argument in lowercase.
 // example input: getSmallerSegment("Codeup", 3)
 // example output: "cod"
+
+function getSmallerSegment(input){
+
+}
+
