278930bc7cb7d5b3bbc552d7c69b01dab46d283c
Julia Contreras <juliacontreras483@gmail.com>
2020-05-06 09:05:00 -0500

    dom-query

---
+++ js/warm-ups.js
@@ -0,0 +1,15 @@
+// Create a function that takes in a array input and returns true if one of the elements is a number that matches the number of total elements in the array.
+//     Example input: [1]
+// Example output: true
+// Example input: ['bob', 3, false]
+// Example output: true
+// Example input: [{'a': 'hello'}, 10, true]
+// Example output: false
+// Example input: [5, {'a': 'hello'}, 30, true, null]
+// Example output: true
+
+
+
+function number () {
+
+}
